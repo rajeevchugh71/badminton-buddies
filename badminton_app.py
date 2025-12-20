0a4e829aa59a99e1403f46877c305de2cf08b22d
Rajeev <chughr@outlook.com>
2025-12-20 18:53:39 +0100

    yet another one

--- badminton_app.py
+++ badminton_app.py
@@ -167,9 +167,9 @@ if (user_role == "Reporting User" or user_role == "Admin") and authorized:
             for session in month_sessions:
                 for player in session['attendees']:
                     if player not in report_card:
-                        report_card[player] = {"Sessions": 0, "Owes (€)": 0.0}
+                        report_card[player] = {"Games": 0, "Owes (€)": 0.0}
                     
-                    report_card[player]["games"] += 1
+                    report_card[player]["Games"] += 1
                     report_card[player]["Owes (€)"] += session['cost_per_person']
             
             df_summary = pd.DataFrame.from_dict(report_card, orient='index')
